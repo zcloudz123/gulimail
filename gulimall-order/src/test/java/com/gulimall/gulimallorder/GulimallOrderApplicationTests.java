ec8cbde5dd514b54e0f9d9f8c15100aaf08849ad
zyy_sys <zyyemail_sys@qq.com>
2020-06-04 16:33:36 +0800

    项目初始微服务架构创建

---
+++ gulimall-order/src/test/java/com/gulimall/gulimallorder/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulimall.gulimallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
