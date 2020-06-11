28ec5c87c9747a761e5636a11ac7533edc932152
zyy_sys <zyyemail_sys@qq.com>
2020-06-11 15:49:57 +0800

    产品微服务：品牌查询提供模糊查询

--- gulimall-product/src/main/java/com/gulimall/product/service/impl/BrandServiceImpl.java
+++ gulimall-product/src/main/java/com/gulimall/product/service/impl/BrandServiceImpl.java
@@ -11,6 +11,7 @@ import com.gulimall.common.utils.Query;
 import com.gulimall.product.dao.BrandDao;
 import com.gulimall.product.entity.BrandEntity;
 import com.gulimall.product.service.BrandService;
+import org.springframework.util.StringUtils;
 
 
 @Service("brandService")
@@ -18,9 +19,16 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        String key = (String) params.get("key");
+        QueryWrapper<BrandEntity> wrapper = new QueryWrapper<>();
+        if(!StringUtils.isEmpty(key)){
+            wrapper.eq("brand_id",key).or().like("name",key);
+        }
+
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
-                new QueryWrapper<BrandEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
