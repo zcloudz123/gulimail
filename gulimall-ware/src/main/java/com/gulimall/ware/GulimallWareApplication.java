59b709f66dcaa86895531411e2c76f932f8d7e4c
zyy_sys <zyyemail_sys@qq.com>
2020-06-12 21:42:41 +0800

    仓库微服务： 1.服务注册和网关路由 2.查询接口支持模糊查询和条件查询

--- gulimall-ware/src/main/java/com/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/gulimall/ware/GulimallWareApplication.java
@@ -1,9 +1,15 @@
 package com.gulimall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
+@EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan(basePackages = "com.gulimall.ware.dao")
 public class GulimallWareApplication {
 
     public static void main(String[] args) {
--- gulimall-ware/src/main/java/com/gulimall/ware/service/impl/WareSkuServiceImpl.java
+++ gulimall-ware/src/main/java/com/gulimall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,5 +1,6 @@
 package com.gulimall.ware.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,9 +19,21 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareSkuEntity> wrapper = new QueryWrapper<>();
+
+        String skuId = (String) params.get("skuId");
+        if(!StringUtils.isEmpty(skuId) && !"0".equalsIgnoreCase(skuId)){
+            wrapper.eq("sku_id",skuId);
+        }
+
+        String wareId = (String) params.get("wareId");
+        if(!StringUtils.isEmpty(wareId) && !"0".equalsIgnoreCase(wareId)){
+            wrapper.eq("ware_id",wareId);
+        }
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
