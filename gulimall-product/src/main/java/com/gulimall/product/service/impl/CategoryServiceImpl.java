22648fa1e1eb73d7d431f83f44c7e3af206c4d8f
zyy_sys <zyyemail_sys@qq.com>
2020-06-16 19:40:14 +0800

    产品微服务： 1.引入spring提供的缓存抽象，使用redis作为缓存实现，序列化器使用JSON 2.为分类查询添加注解缓存 3.配置过期时间、分组使用key，可缓存空值

--- gulimall-product/src/main/java/com/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -17,6 +17,8 @@ import org.apache.commons.lang.BooleanUtils;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -94,6 +96,7 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return paths.toArray(new Long[]{});
     }
 
+    @CacheEvict(cacheNames = {"category"}, allEntries = true)
     @Transactional
     @Override
     public void updateCascade(CategoryEntity category) {
@@ -101,74 +104,45 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         categoryBrandRelationService.updateCategoryName(category.getCatId(), category.getName());
     }
 
+    @Cacheable(cacheNames = {"category"}, key = "#root.method.name",sync = true)
     @Override
     public List<CategoryEntity> getLevel1Category() {
+        System.out.println("查询DB1级分类");
         return this.list(new QueryWrapper<CategoryEntity>().eq("parent_cid", 0));
     }
 
+    @Cacheable(cacheNames = {"category"}, key = "#root.method.name",sync = true)
     @Override
     public Map<String, List<Catalog2Vo>> getCatalogJson() {
-        //redis缓存时使用JSON字符串保存对象，可实现跨平台
-        ValueOperations<String, String> ops = redisTemplate.opsForValue();
-        String catalogJson = ops.get("catalogJson");
-        if (StringUtils.isEmpty(catalogJson)) {
-            return this.getCatalogJsonFromDb();
-        }
-        return JSON.parseObject(catalogJson, new TypeReference<Map<String, List<Catalog2Vo>>>() {
-        });
-    }
 
-    private Map<String, List<Catalog2Vo>> getCatalogJsonFromDb() {
+        System.out.println("查询了DB");
+        //优化业务，一次查询所有分类，再统一处理
+        List<CategoryEntity> list = this.list();
 
-        //TODO 本地锁 synchronized JUC(lock)在分布式情况下必须使用分布式锁
-        RLock lock = redissonClient.getLock("CatalogJson-lock");
-        ValueOperations<String, String> ops = redisTemplate.opsForValue();
-        Map<String, List<Catalog2Vo>> map;
-        //占分布式锁
-        lock.lock();
-        try {
-            //加锁后要二次确认
-            String catalogJson = ops.get("catalogJson");
-            if (catalogJson != null) {
-                return JSON.parseObject(catalogJson, new TypeReference<Map<String, List<Catalog2Vo>>>() {
-                });
-            }
-            System.out.println("查询了DB");
-
-            //优化业务，一次查询所有分类，再统一处理
-            List<CategoryEntity> list = this.list();
-
-            List<CategoryEntity> categoryLevel1 = getParent_cid(list, 0L);
-            map = categoryLevel1.stream().collect(Collectors.toMap(
-                    cat -> cat.getCatId().toString(),
-                    cat -> {
-                        //遍历每个一级分类，查找所有2级分类，封装到VO
-                        List<CategoryEntity> catalog2Entites = getParent_cid(list, cat.getCatId());
-                        return catalog2Entites.stream().map(entity -> {
-                            Catalog2Vo catalog2Vo = new Catalog2Vo();
-                            catalog2Vo.setCatalog1Id(entity.getParentCid());
-                            catalog2Vo.setId(entity.getCatId());
-                            catalog2Vo.setName(entity.getName());
-                            //遍历每一个2级分类，查询封装3级分类
-                            List<Catalog3Vo> catalog3Vos = getParent_cid(list, entity.getCatId()).stream().map(
-                                    entity1 -> {
-                                        Catalog3Vo catalog3Vo = new Catalog3Vo();
-                                        catalog3Vo.setId(entity1.getCatId());
-                                        catalog3Vo.setCatalog2Id(entity1.getParentCid());
-                                        catalog3Vo.setName(entity1.getName());
-                                        return catalog3Vo;
-                                    }).collect(Collectors.toList());
-                            catalog2Vo.setCatalog3List(catalog3Vos);
-                            return catalog2Vo;
-                        }).collect(Collectors.toList());
-                    }));
-            String mapS = JSON.toJSONString(map);
-            ops.set("catalogJson", mapS);
-        } finally {
-            lock.unlock();
-        }
-        return map;
-
+        List<CategoryEntity> categoryLevel1 = getParent_cid(list, 0L);
+        return categoryLevel1.stream().collect(Collectors.toMap(
+                cat -> cat.getCatId().toString(),
+                cat -> {
+                    //遍历每个一级分类，查找所有2级分类，封装到VO
+                    List<CategoryEntity> catalog2Entites = getParent_cid(list, cat.getCatId());
+                    return catalog2Entites.stream().map(entity -> {
+                        Catalog2Vo catalog2Vo = new Catalog2Vo();
+                        catalog2Vo.setCatalog1Id(entity.getParentCid());
+                        catalog2Vo.setId(entity.getCatId());
+                        catalog2Vo.setName(entity.getName());
+                        //遍历每一个2级分类，查询封装3级分类
+                        List<Catalog3Vo> catalog3Vos = getParent_cid(list, entity.getCatId()).stream().map(
+                                entity1 -> {
+                                    Catalog3Vo catalog3Vo = new Catalog3Vo();
+                                    catalog3Vo.setId(entity1.getCatId());
+                                    catalog3Vo.setCatalog2Id(entity1.getParentCid());
+                                    catalog3Vo.setName(entity1.getName());
+                                    return catalog3Vo;
+                                }).collect(Collectors.toList());
+                        catalog2Vo.setCatalog3List(catalog3Vos);
+                        return catalog2Vo;
+                    }).collect(Collectors.toList());
+                }));
     }
 
     private List<CategoryEntity> getParent_cid(List<CategoryEntity> list, Long parentCid) {
