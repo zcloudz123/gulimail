68b58928c7ef34ffd6ba4292a4868c3603b54ec8
zyy_sys <zyyemail_sys@qq.com>
2020-07-02 11:57:12 +0800

    网关微服务： 1.整合Sentinel-Gateway，配置限流后返回数据

---
+++ gulimall-gateway/src/main/java/com/gulimall/gateway/config/SentinelGatewayConfig.java
@@ -0,0 +1,31 @@
+package com.gulimall.gateway.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.fastjson.JSON;
+import com.gulimall.common.exception.BizCodeEnum;
+import com.gulimall.common.utils.R;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @decription:
+ * @author: zyy
+ * @date 2020-07-02-11:37
+ */
+//响应式编程
+@Configuration
+public class SentinelGatewayConfig {
+    public SentinelGatewayConfig() {
+        GatewayCallbackManager.setBlockHandler(new BlockRequestHandler() {
+            @Override
+            public Mono<ServerResponse> handleRequest(ServerWebExchange serverWebExchange, Throwable throwable) {
+                R error = R.error(BizCodeEnum.TOO_MANY_REQUEST.getCode(), BizCodeEnum.TOO_MANY_REQUEST.getMsg());
+                String errJson = JSON.toJSONString(error);
+                return ServerResponse.ok().body(Mono.just(errJson),String.class);
+            }
+        });
+    }
+}
